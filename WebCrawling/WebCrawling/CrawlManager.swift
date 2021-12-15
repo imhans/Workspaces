078405fec034b54b868359f6ea3261c6a9ab4b30
hanzy <hans78905@gmail.com>
2021-12-15 22:43:55 +0900

    Set Cocoapod Dependencies: Alamofire, SwiftSoup for Web Crawling and Scraping

---
+++ WebCrawling/WebCrawling/CrawlManager.swift
@@ -0,0 +1,4 @@
+import Foundation
+import SwiftSoup
+import Alamofire
+
---
+++ WebCrawling/WebCrawling/WebCrawlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebCrawlingApp.swift
+//  WebCrawling
+//
+//  Created by Hans Yim on 2021/12/15.
+//
+
+import SwiftUI
+
+@main
+struct WebCrawlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
