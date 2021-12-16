a12f4ea1e6e28e5a99060dd347403d3b96e18661
hanzy <hans78905@gmail.com>
2021-12-17 00:07:03 +0900

    Get the list of data by parsing

--- WebCrawling/WebCrawling/CrawlManager.swift
+++ WebCrawling/WebCrawling/CrawlManager.swift
@@ -10,12 +10,24 @@ func fetchHTML() {
         guard let html = response.value else {
             return
         }
-    }
-    
-    //Get data by parsing the HTML Source
-    do {
+        
+        //Get data by parsing the HTML Source
+        do {
+            let doc: Document = try SwiftSoup.parse(html)
+            
+            // #list-article > div.panel.panel-default.community-panel > ul > li:nth-child(1) > div.list-title-wrapper.clearfix > h5 > a
+            let elements: Elements = try doc.select("#list-article > div.panel.panel-default.community-panel > ul > li > div.list-title-wrapper.clearfix")
+            for element in elements {
+                print(try element.select("h5 > a").text())
+            }
+            print("\(elements)")
+        } catch {
+            print("Crawl Error: ", error)
+        }
         
     }
+    
+    
 }
 
 
