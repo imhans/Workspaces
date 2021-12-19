49aeb4e1630ff35ea758967871ddbc1a425858fc
hanzy <hans78905@gmail.com>
2021-12-19 20:10:31 +0900

    Add the parsed data to 2D Array that is a data set

--- WebCrawling/WebCrawling/CrawlManager.swift
+++ WebCrawling/WebCrawling/CrawlManager.swift
@@ -11,6 +11,8 @@ func fetchHTML() {
             return
         }
         
+        var lines: [[String]] = [] //2D Array as a data set
+        
         //Get data by parsing the HTML Source
         do {
             let doc: Document = try SwiftSoup.parse(html)
@@ -18,9 +20,14 @@ func fetchHTML() {
             // #list-article > div.panel.panel-default.community-panel > ul > li:nth-child(1) > div.list-title-wrapper.clearfix > h5 > a
             let elements: Elements = try doc.select("#list-article > div.panel.panel-default.community-panel > ul > li > div.list-title-wrapper.clearfix")
             for element in elements {
-                print(try element.select("h5 > a").text())
+                let id: String = try element.select("div.list-tag > span").text()
+                let article: String = try element.select("h5 > a").text()
+                
+                //Each element of the array corresponds to a column of a data set
+                let line: [String] = [id, article]
+                lines.append(line)
             }
-            print("\(elements)")
+            print("\(lines)")
         } catch {
             print("Crawl Error: ", error)
         }
