96ee5823f2ce2d3b0847522f336298d429f4f503
hanzy <hans78905@gmail.com>
2022-02-03 22:54:54 +0900

    BasicDatabase: Add Firebase to the project

---
+++ BasicDatabaseFirebase/BasicDatabaseFirebase/BasicDatabaseFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicDatabaseFirebaseApp.swift
+//  BasicDatabaseFirebase
+//
+//  Created by Hans Yim on 2022/02/03.
+//
+
+import SwiftUI
+
+@main
+struct BasicDatabaseFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
